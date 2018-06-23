c7c4dda04b9bd4c85499a25ad938000d775837b2
Yesh Chandiramani <yesh0907@hotmail.com>
2018-06-24 04:46:12 +0800

    connect backend and frontend

--- src/Components/Listings.js
+++ src/Components/Listings.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
 import { Row, Col, Card, Icon } from 'antd';
 import IndivListing from './IndivListing';
+import axios from 'axios';
+import BASE_URL from '../HttpConstants.js';
 
 class Listings extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
+
+  componentWillMount() {
+    axios.get(BASE_URL + "items/").then((res) => {
+      this.setState({ data: res.data });
+    });
+  }
+
   render() {
+    const ListView = this.state.data.map((item, index) => {
+      return (
+        <Col span={6} key={index}>
+          <IndivListing data={item} />
+        </Col>
+      );
+    });
+
     return (
       <div id="listings">
         <h5 id="listingsTitle">Current <span class="share">Listings</span></h5>
           <div style={{ padding: '50px'}}>
             <Row gutter={24}>
-              <Col span={6}>
-                <IndivListing/>
-              </Col>
-              <Col span={6}>
-                <IndivListing/>
-              </Col>
-              <Col span={6}>
-                <IndivListing/>
-              </Col>
-              <Col span={6}>
-                <IndivListing/>
-              </Col>
+              {ListView}
             </Row>
           </div>
       </div>
---
+++ src/HttpConstants.js
@@ -0,0 +1,2 @@
+const kBASE_URL = "http://192.168.0.180:8000/";
+export default kBASE_URL
